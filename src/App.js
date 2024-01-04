d0dfb263c81d287f96e4bf221f71109cec761821
Mohd Azgar Ali <azgar53ali@gmail.com>
2024-01-04 10:43:09 +0530

    updated the functionality of name display

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { useState } from 'react';
 
 function App() {
   const[fname, setFname] = useState({firstName:'',lastName:''});
+  const [nameDisplayed, setNameDisplayed] = useState(false)
+
 
   const handleChange=(event)=>{
     const {name, value}=event.target
     setFname((prev)=>({...prev, [name]: value}) )
   }
 
+  const hideName=()=> setNameDisplayed(false);
  
 
   const handleSubmit=(event)=>{
     event.preventDefault();
-    const name = document.getElementById('nameDisplay');
-    name.innerHTML= `Full Name: ${fname.firstName} ${fname.lastName}`
+    setNameDisplayed(true);
   }
 
   return (
@@ -22,12 +24,12 @@ function App() {
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor='firstName'>First Name:</label>
-        <input type='text' required value={fname.firstName} id='firstName' onChange={handleChange} name='firstName'/><br/>
+        <input type='text' required value={fname.firstName} id='firstName' onChange={handleChange} onFocus={hideName} name='firstName'/><br/>
         <label htmlFor='lastName'>Last Name:</label>
-        <input type='text' required value={fname.lastName} id='lastName' onChange={handleChange} name='lastName'/><br/>
+        <input type='text' required value={fname.lastName} id='lastName' onChange={handleChange} onFocus={hideName} name='lastName'/><br/>
         <button type='submit'>Submit</button>
       </form>
-      <p id='nameDisplay'></p>
+      {nameDisplayed?<p id='nameDisplay'>Full Name: {`${fname.firstName} ${fname.lastName}`}</p>:' '}
     </div>
   );
 }
